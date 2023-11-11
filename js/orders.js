4704fee1e9c60e06b60fb41b1eb15cb21c3fb195
VICTORIN Laurent <laurentvictorin33@gmail.com>
2023-11-11 09:51:53 +0100

    Add files via upload

---
+++ js/orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+  {
+    productName: "BEESLEY Loïs",
+    productNumber: "166€",
+    paymentStatus: "en ligne",
+    status: "Effectué",
+  },
+  {
+    productName: "DUQUESNOIS Nans",
+    productNumber: "206€",
+    paymentStatus: "En ligne",
+    status: "En cours",
+  },
+  {
+    productName: "VICTORIN Laurent",
+    productNumber: "0€",
+    paymentStatus: "en ligne",
+    status: "Problème",
+  },
+];
