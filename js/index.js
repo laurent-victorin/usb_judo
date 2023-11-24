29edd2acac2fb962b7636e427efb1a96a907655d
VICTORIN Laurent <laurentvictorin33@gmail.com>
2023-11-24 23:19:59 +0100

    Version 1.09, Dark Thème Mode

--- js/index.js
+++ js/index.js
@@ -1,7 +1,19 @@
-const darkMode = document.querySelector(".dark-mode");
+// Switch function
+const switchTheme = () => {
+  // Get Root Element and the data-theme value
+  const rootElem = document.documentElement;
+  let dataTheme = rootElem.getAttribute("data-theme"),
+    newTheme;
 
-darkMode.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode-variables");
-  darkMode.querySelector("span:nth-child(1)").classList.toggle("active");
-  darkMode.querySelector("span:nth-child(2)").classList.toggle("active");
-});
+  newTheme = dataTheme === "light" ? "dark" : "light";
+
+  // Set the new HTML attribute
+  rootElem.setAttribute("data-theme", newTheme);
+};
+
+// Add EventElement
+document
+  .querySelector("#theme-switcher")
+  .addEventListener("click", switchTheme);
+
+// https://youtu.be/Xk12JtYG8rw?si=xV4m2OQGTkyC_ONC
---
+++ js/slider-img-header.js
@@ -0,0 +1,77 @@
+const app = {
+  sliderContainer: document.querySelector(".slider"),
+  sliderBtns: document.querySelectorAll(".slider button"),
+  sliderPhotographyNames: [
+    "slider_judo_01",
+    "slider_judo_02",
+    "slider_judo_03",
+    "slider_judo_04",
+    "slider_judo_05",
+    "slider_judo_06",
+  ],
+  sliderIndex: 0,
+
+  initSlider: function () {
+    const previousBtn = app.sliderBtns[0];
+    const nextBtn = app.sliderBtns[1];
+
+    nextBtn.addEventListener("click", function () {
+      app.showNextImage();
+    });
+
+    previousBtn.addEventListener("click", function () {
+      app.showPreviousImage();
+      console.log(1);
+    });
+
+    // * déclenche le changement d'image toutes les 3 secondes
+    setInterval(function () {
+      app.showNextImage();
+    }, 3000);
+
+    // * On met en place la première image du slider
+    console.log(2);
+    app.switchSliderImage();
+  },
+
+  switchSliderImage: function () {
+    const image = document.createElement("img");
+
+    // * Si on arrive au bout du tableau, on réinitialise l'index
+    if (app.sliderIndex > app.sliderPhotographyNames.length - 1) {
+      app.sliderIndex = 0;
+    }
+
+    // * Si on arrive au début du tableau, on met l'index en dernière position du tableau
+    if (app.sliderIndex < 0) {
+      app.sliderIndex = app.sliderPhotographyNames.length - 1;
+    }
+
+    // image.src = `img/${app.sliderPhotographyNames[app.sliderIndex]}.jpg`;
+
+    image.setAttribute(
+      "src",
+      `images/${app.sliderPhotographyNames[app.sliderIndex]}.jpg`
+    );
+
+    image.classList.add("slider__img", "slider__img--current");
+    app.sliderContainer.appendChild(image);
+  },
+
+  showNextImage: function () {
+    app.sliderIndex += 1;
+    app.switchSliderImage();
+  },
+
+  showPreviousImage: function () {
+    app.sliderIndex -= 1;
+    app.switchSliderImage();
+  },
+
+  init() {
+    app.initSlider();
+  },
+}; // fin App
+
+// On attend que le HTML soit parsé pour démarrer notre application
+document.addEventListener("DOMContentLoaded", app.init);
